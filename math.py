4feebaa2db4c78b5d94d3508cfe4f8c664d9e41f
Shayonis <128481366+Shayonis@users.noreply.github.com>
2023-03-21 12:56:29 +0100

    added math.py

---
+++ math.py
@@ -0,0 +1,43 @@
+import math
+
+def addieren(zahlen):
+    ergebnis = 0
+    for zahl in zahlen:
+        ergebnis += zahl
+    return ergebnis
+
+def subtrahieren(zahlen):
+    ergebnis = zahlen[0]
+    for i in range (1, len(zahlen)):
+        ergebnis -= zahlen[1]
+    return ergebnis
+
+def multiplizieren(zahlen):
+    ergebnis = 1
+    for zahl in zahlen:
+        ergebnis *= zahl
+    return ergebnis
+
+def dividieren(zahlen):
+    ergebnis = zahlen[0]
+    for i in range(1, len(zahlen)):
+        ergebnis /= zahlen[i]
+    return ergebnis
+
+def quadrieren(zahlen):
+    ergebnis = [] #später gucken ob klappt
+    for zahl in zahlen:
+        ergebnis.append(zahl**2)
+    return ergebnis
+
+def wurzel(zahlen):
+    ergebnis = [] #""""""
+    for zahl in zahlen:
+        ergebnis.append(math.sqrt(zahl))
+    return ergebnis
+
+def potenz(zahlen):
+    result = zahlen[0]
+    for zahl in zahlen[1:]:
+        ergebnis = ergebnis ** zahl
+    return ergebnis
