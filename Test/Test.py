32e6ce0613858e5e650e1e67288a3219131d04f2
SchissTV <114029138+SchissTV@users.noreply.github.com>
2023-03-22 14:20:10 +0100

    t

---
+++ Test/Test.py
@@ -0,0 +1,59 @@
+def split_taschenrechner(rechnung):
+    zahlen = []
+    operatoren = []
+    zahl = ""
+    for i in rechnung:
+        if i.isdigit() or i == ".":
+            zahl += i 
+        elif i in ["+","-","*","/","**2"]:
+            if zahl:
+                zahlen.append(float(zahl))
+                zahl = ""
+            operatoren.append(i)
+    if zahl:
+        zahlen.append(float(zahl))
+
+    # Sortiere Operatoren nach Reihenfolge der Operationen
+    sorted_operators = []
+    for op in ["**2", "/", "*", "+", "-"]:
+        if op in operatoren:
+            sorted_operators.append(op)
+
+    return zahlen, sorted_operators
+
+def berechne(rechnung):
+    zahlen, operatoren = split_taschenrechner(rechnung)
+
+    # Führe die Operationen in der richtigen Reihenfolge aus
+    while "**2" in operatoren:
+        idx = operatoren.index("**2")
+        zahlen[idx] = zahlen[idx]**2
+        del operatoren[idx]
+
+    while "*" in operatoren:
+        idx = operatoren.index("*")
+        zahlen[idx] = zahlen[idx] * zahlen[idx+1]
+        del zahlen[idx+1]
+        del operatoren[idx]
+
+    while "/" in operatoren:
+        idx = operatoren.index("/")
+        if zahlen[idx+1] == 0:
+            raise ZeroDivisionError("Division durch Null ist nicht definiert")
+        zahlen[idx] = zahlen[idx] / zahlen[idx+1]
+        del zahlen[idx+1]
+        del operatoren[idx]
+
+    while "+" in operatoren:
+        idx = operatoren.index("+")
+        zahlen[idx] = zahlen[idx] + zahlen[idx+1]
+        del zahlen[idx+1]
+        del operatoren[idx]
+
+    while "-" in operatoren:
+        idx = operatoren.index("-")
+        zahlen[idx] = zahlen[idx] - zahlen[idx+1]
+        del zahlen[idx+1]
+        del operatoren[idx]
+
+    return zahlen[0]
