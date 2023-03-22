d50e0e225e0b25398da89603028f920a5c3d8351
SkarlaxRobotics <111658819+SkarlaxRobotics@users.noreply.github.com>
2023-03-22 12:40:18 +0100

    debug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import flask
 from flask import Flask, render_template, url_for, request
-import math_own
+#import math_own
 
 def rechner():
     x1 = int(input("Dein erste Zahl:"))
@@ -29,7 +29,8 @@ def result():
     print(output)
     #ergebnis = output["num"]
     first_digit = float(output["fdigit"])
-    math_own.main(first_digit)
+    #math_own.main(first_digit)
+    print(first_digit)
     #second_digit = float(output["sdigit"])
     #operator = str(output["operator"])
     #ergebnis = first_digit*second_digit
