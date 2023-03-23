22c1f6c1d65bca16efe1e0281b04b03bca7b7754
SchissTV <114029138+SchissTV@users.noreply.github.com>
2023-03-23 14:47:14 +0100

    s

--- math_own.py
+++ math_own.py
@@ -1,4 +1,5 @@
 import math
+import re
 # def split_taschenrechner(rechnung):
 #     zahlen = []
 #     operatoren = []
@@ -19,6 +20,24 @@ import math
 #         zahlen.append(float(zahl))
 #     print(zahlen, operatoren)
 #     return zahlen, operatoren
+
+def regu(rechnung):
+
+    regex = r"([a-zA-Z])|(-?\d+(\.\d+)?)|([+\-*/^()])|([^\d])"gm
+
+    test_str = rechnung
+
+    matches = re.finditer(regex, test_str, re.MULTILINE)
+
+    for matchNum, match in enumerate(matches, start=1):
+        
+        print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+        
+        for groupNum in range(0, len(match.groups())):
+            groupNum = groupNum + 1
+            
+            print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+
 
 def split_taschenrechner(rechnung):
     zahlen = []
