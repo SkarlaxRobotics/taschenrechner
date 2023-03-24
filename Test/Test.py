d2386cd81cc37abacd06ed5e12529ef36e33d579
SchissTV <114029138+SchissTV@users.noreply.github.com>
2023-03-24 13:16:59 +0100

    s

--- Test/Test.py
+++ Test/Test.py
@@ -27,28 +27,6 @@ def split_taschenrechner(rechnung):
     print(zahlen, operatoren)
     return zahlen, operatoren
 
-def split_taschenrechner(rechnung):
-    zahlen = []
-    operatoren = []
-    zahl = ""
-    o = ["+", "-", "*", "/", "^"]
-    last_was_op = True # flag to check if the last character was an operator
-    for i in rechnung:
-        if i.isdigit() or i == ".":
-            zahl += i 
-            last_was_op = False
-        elif i in o:
-            if zahl:
-                zahlen.append(float(zahl))
-                zahl = ""
-            if not last_was_op:
-                operatoren.append("+")
-            operatoren.append(i)
-            last_was_op = True
-    if zahl:
-        zahlen.append(float(zahl))
-    return zahlen, operatoren
-
 
 
 def main(rechnung):
@@ -57,19 +35,4 @@ def main(rechnung):
     ergebnis = loese(rechnung)
     return ergebnis
     
-def loese(rechnung):
-    zahlen, operatoren = split_taschenrechner(rechnung)
-    
-    while "!" in operatoren:
-        for i, op in enumerate(operatoren):
-            if op == "!":
-                    
-                    if i == "0":
-                        zahlen[i]=1
-                        del operatoren[i]
-                    else:
-                        zahlen[i]=math.factorial(int(zahlen[i]))
-                        del operatoren[i]
-                # else:
-                #     return "n muss eine positive ganze Zahl oder Null sein"
 
