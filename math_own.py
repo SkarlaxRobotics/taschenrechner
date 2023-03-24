f473dd99e25ff3fc0f8bf639766d69774aef6be6
Shayonis <128481366+Shayonis@users.noreply.github.com>
2023-03-24 08:51:49 +0100

    eingabe

--- math_own.py
+++ math_own.py
@@ -1,11 +1,52 @@
 import math
+import re
+# def split_taschenrechner(rechnung):
+#     zahlen = []
+#     operatoren = []
+#     zahl = ""
+#     o = ["+","-","*","/","^"]
+#     for i in rechnung:
+#         if i.isdigit() or i == ".":
+#             zahl += i 
+#         elif i == "-"  and i-1 in o:
+#             zahl += "-"
+        
+#         elif i in o:
+#             if zahl:
+#                 zahlen.append(float(zahl))
+#                 zahl = ""
+#             operatoren.append(i)
+#     if zahl:
+#         zahlen.append(float(zahl))
+#     print(zahlen, operatoren)
+#     return zahlen, operatoren
+
+def regu(rechnung):
+    regex = r"\\(- [0-9]+ \\+ n\\) \\* [0-9]+ \\^ [0-9]+ \\^ [0-9]+ - - [0-9]+"
+
+    matches = re.finditer(regex, rechnung)
+
+    zahlen = []
+    operatoren = []
+
+    for match in matches:
+        for group in range(1, 5):
+            token = match.group(group)
+            if token:
+                if token.isnumeric() or '.' in token:
+                    zahlen.append(token)
+                else:
+                    operatoren.append(token)
+
+    # Ausgabe der Zahlen und Operatoren
+    print("Zahlen:", zahlen)
+    print("Operatoren:", operatoren)
 
 def split_taschenrechner(rechnung):
     zahlen = []
     operatoren = []
     zahl = ""
     x="%s" % math.pi
-
     for i in rechnung:
         if i.isdigit() or i == "." :
             zahl += i 
@@ -21,6 +62,8 @@ def split_taschenrechner(rechnung):
     return zahlen, operatoren
 
 def main(rechnung):
+    if not rechnung:
+        return "Bitte Eingabe"
     ergebnis = loese(rechnung)
     return ergebnis
     
