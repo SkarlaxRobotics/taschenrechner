3fe90a51c217d09d410aff300b2e66896d6c8aa2
SchissTV <114029138+SchissTV@users.noreply.github.com>
2023-03-24 08:53:26 +0100

    Test

--- Test/Test.py
+++ Test/Test.py
@@ -1,56 +1,35 @@
+import re
 def split_taschenrechner(rechnung):
     zahlen = []
     operatoren = []
     zahl = ""
+    o = ["+","-","*","/","^"]
     for i in rechnung:
         if i.isdigit() or i == ".":
             zahl += i 
-        elif i in ["+","-","*","/","**2"]:
+        elif i == "-"  and i-1 in o:
+            zahl += "-"
+        
+        elif i in o:
             if zahl:
                 zahlen.append(float(zahl))
                 zahl = ""
             operatoren.append(i)
     if zahl:
         zahlen.append(float(zahl))
+    print(zahlen, operatoren)
+    return zahlen, operatoren
 
-    # Sortiere Operatoren nach Reihenfolge der Operationen
-    sorted_operators = []
-    for op in ["**2", "/", "*", "+", "-"]:
-        if op in operatoren:
-            sorted_operators.append(op)
+def split_operands(expression):
+    regex = r'([a-zA-Z]*)(\d+)([^\w\s]*)'
+    matches = re.findall(regex, expression)
+    results = []
+    for match in matches:
+        if match[0]:
+            results.append((match[0], None))
+        results.append((match[1], match[2]))
+    return results
 
-    return zahlen, sorted_operators
-
-def berechne(rechnung):
-    zahlen, operatoren = split_taschenrechner(rechnung)
-
-    # Führe die Operationen in der richtigen Reihenfolge aus
-    while "**2" in operatoren:
-        idx = operatoren.index("**2")
-        zahlen[idx] = zahlen[idx]**2
-        del operatoren[idx]
-
-    while "*"  or "/" in operatoren:
-
-        if "*":
-            
-
-        idx = operatoren.index("*")
-        zahlen[idx] = zahlen[idx] * zahlen[idx+1]
-        del zahlen[idx+1]
-        del operatoren[idx]
-
-
-    while "+" in operatoren:
-        idx = operatoren.index("+")
-        zahlen[idx] = zahlen[idx] + zahlen[idx+1]
-        del zahlen[idx+1]
-        del operatoren[idx]
-
-    while "-" in operatoren:
-        idx = operatoren.index("-")
-        zahlen[idx] = zahlen[idx] - zahlen[idx+1]
-        del zahlen[idx+1]
-        del operatoren[idx]
-
-    return zahlen[0]
+    # Ausgabe der Zahlen und Operatoren
+    print("Zahlen:", zahlen)
+    print("Operatoren:", operatoren)
