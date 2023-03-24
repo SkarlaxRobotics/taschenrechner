9389b5023c96e4c4d1078148cc87ff9ce71ed8ce
SchissTV <114029138+SchissTV@users.noreply.github.com>
2023-03-24 09:14:28 +0100

    d

--- Test/Test.py
+++ Test/Test.py
@@ -1,26 +1,45 @@
 import re
-# def split_taschenrechner(rechnung):
-#     zahlen = []
-#     operatoren = []
-#     zahl = ""
-#     o = ["+","-","*","/","^"]
-#     for i in rechnung:
-#         if i.isdigit() or i == ".":
-#             zahl += i 
-#         elif i == "-"  and i-1 in o:
-#             zahl += "-"
+def split_taschenrechner(rechnung):
+    zahlen = []
+    operatoren = []
+    zahl = ""
+    o = ["+","-","*","/","^"]
+    for i in rechnung:
+        if i.isdigit() or i == ".":
+            zahl += i 
+        elif i == "-"  and i-1 in o:
+            zahl += "-"
         
-#         elif i in o:
-#             if zahl:
-#                 zahlen.append(float(zahl))
-#                 zahl = ""
-#             operatoren.append(i)
-#     if zahl:
-#         zahlen.append(float(zahl))
-#     print(zahlen, operatoren)
-#     return zahlen, operatoren
+        elif i in o:
+            if zahl:
+                zahlen.append(float(zahl))
+                zahl = ""
+            operatoren.append(i)
+    if zahl:
+        zahlen.append(float(zahl))
+    print(zahlen, operatoren)
+    return zahlen, operatoren
 
+def regu(rechnung):
+    regex = r""
 
+    matches = re.finditer(regex, rechnung)
+
+    zahlen = []
+    operatoren = []
+
+    for match in matches:
+        for group in range(1, 5):
+            token = match.group(group)
+            if token:
+                if token.isnumeric() or '.' in token:
+                    zahlen.append(token)
+                else:
+                    operatoren.append(token)
+
+    # Ausgabe der Zahlen und Operatoren
+    print("Zahlen:", zahlen)
+    print("Operatoren:", operatoren)
 
 
 
