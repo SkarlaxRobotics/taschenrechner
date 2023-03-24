51575c724a115ab91fe53ad467ace98d6803017a
SchissTV <114029138+SchissTV@users.noreply.github.com>
2023-03-24 13:01:03 +0100

    s

--- Test/Test.py
+++ Test/Test.py
@@ -1,28 +1,31 @@
 import re
 import math
-def split_taschenrechner1(rechnung):
+def split_taschenrechner(rechnung):
     zahlen = []
     operatoren = []
     zahl = ""
-    o = ["+","-","*","/","^"]
+    o = ["+","-","*","/","^", "!"]
+    x=True
     for i in rechnung:
-        z=rechnung[rechnung.index(i)-3]
-        print(i)
-        print(z)
-        if i.isdigit() or i == ".":
+        if i.isdigit() or i == "." :
             zahl += i 
-        elif i == "-"  and z in o:
+        elif i == "-"  and x==True:
             zahl += "-"
         elif i in o:
             if zahl:
                 zahlen.append(float(zahl))
                 zahl = ""
             operatoren.append(i)
+        if i in o:
+            x=True
+        elif x==True and i==" ":
+            x=True
+        else:
+            x=False
     if zahl:
         zahlen.append(float(zahl))
     print(zahlen, operatoren)
     return zahlen, operatoren
-
 
 def split_taschenrechner(rechnung):
     zahlen = []
