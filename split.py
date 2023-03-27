35bc58038bc5d0be4f3f02e3db4d425213e0d2bf
Shayonis <128481366+Shayonis@users.noreply.github.com>
2023-03-27 13:32:52 +0200

    clean new

---
+++ split.py
@@ -0,0 +1,50 @@
+import math
+class split:
+    def split_rechnung(rechnung):
+
+        zahlen = []
+        operatoren = []
+        zahl = ""
+        o = ["+","-","*","/","^", "!"]
+        x=True
+        y=False
+        n="%s" % math.pi 
+        
+        for i in rechnung:
+            if rechnung[-2] in o and rechnung[-2]!="!":
+                return "Syntax Error"
+            elif i.isdigit() or i == ".":
+                zahl += i 
+            elif i=="n":
+                if rechnung[rechnung.index(i)-2] in o:
+                    zahl += n
+                else:
+                    zahlen.append(float(zahl))
+                    operatoren.append("*")
+                    zahl = ""
+                    zahl += n
+            elif (i =="-" or i=="+" )and x==True:
+                zahl += i
+            elif i in o:
+                if zahl:
+                    zahlen.append(float(zahl))
+                    zahl = ""
+                operatoren.append(i)
+            if i in o and y==False and x==False:
+                x=True
+            elif i in o and x==True and y==False:
+                y=True
+            elif i in o and y==True:
+                return "Syntax Error"
+            elif x==True and i==" ":
+                x=True
+            else:
+                x=False
+                y=False
+
+        if zahl:
+            zahlen.append(float(zahl))
+        # print(zahlen, operatoren)
+        return zahlen, operatoren
+
+
