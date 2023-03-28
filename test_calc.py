2f84b9dcd11d56e7a52e33256ad2a9ae06ee9e97
SkarlaxRobotics <111658819+SkarlaxRobotics@users.noreply.github.com>
2023-03-28 10:58:12 +0200

    even more tests

--- test_calc.py
+++ test_calc.py
@@ -18,8 +18,25 @@ def testFakultaet():
 
 def testKlammer():
     result = main("2 * ( 2 + 3 )")
-    assert result == 24
+    assert result == 10
 
 def testFakultaet():
     result = main("4 !")
     assert result == 24
+
+def testPi():
+    result = main("4 * n")
+    assert result == 12.566370614359172
+
+def testPowerof2():
+    result = main("5 ^ 2")
+    assert result == 25
+
+def testRoot():
+    result = main("8 ^ 0.5")
+    assert result == 2.8284271247461903
+
+
+def testPowerofX():
+    result = main("8 ^ 8")
+    assert result == 16777216.0
