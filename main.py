3174273cd1a3988b1d80afe124c43342ddd36d85
SkarlaxRobotics <111658819+SkarlaxRobotics@users.noreply.github.com>
2023-03-29 08:24:56 +0200

    execption handling

--- main.py
+++ main.py
@@ -18,30 +18,32 @@ def index():
 
 @app.route("/result",methods = ['POST', 'GET'])
 def result(debug=False):
-    output = request.form.to_dict()
+    try:
+        output = request.form.to_dict()
     
-    if debug: print(output)
+        if debug: print(output)
     
-    digit = str(output["digit"])
+        digit = str(output["digit"])
 
-    # Berechnung:
-    ergebnis = math_own.main(digit)
+        # Berechnung:
+        ergebnis = math_own.main(digit)
 
-    if debug: print("Ergebnis: ", ergebnis); print("Digit: ", digit)
-    
-    # getting max previous number
-    max_number = db.getMaxNumber()
-    if debug: print(max_number)
-    a_max_number = max_number if max_number is not None else 0
+        if debug: print("Ergebnis: ", ergebnis); print("Digit: ", digit)
+        # getting max previous number
+        max_number = db.getMaxNumber()
+        if debug: print(max_number)
+        a_max_number = max_number if max_number is not None else 0
 
-    # verlauf erstellen
-    if digit == "1 + 1": db.insertToTable(number=int(a_max_number+1), rechnung="Nutzer", ergebnis="doof")
-    else: db.insertToTable(number=int(a_max_number+1), rechnung=digit, ergebnis=ergebnis)
+        # verlauf erstellen
+        if digit == "1 + 1": db.insertToTable(number=int(a_max_number+1), rechnung="Nutzer", ergebnis="doof")
+        else: db.insertToTable(number=int(a_max_number+1), rechnung=digit, ergebnis=ergebnis)
 
-    last_ergebnis = "0" if type == str and ' ' in ergebnis else ergebnis
-    if debug: print(last_ergebnis)
+        last_ergebnis = "0" if type == str and ' ' in ergebnis else ergebnis
+        if debug: print(last_ergebnis)
 
-    return render_template("index.html", value=ergebnis, history=reversed(db.readFromTable("*")), last_ergebnis=last_ergebnis)
+        return render_template("index.html", value=ergebnis, history=reversed(db.readFromTable("*")), last_ergebnis=last_ergebnis)
+    except:
+        return render_template("index.html", value="Unbekannter Fehler", history=reversed(db.readFromTable("*")), last_ergebnis="0")
 
 
 @app.route("/clear",methods = ['POST', 'GET'])
