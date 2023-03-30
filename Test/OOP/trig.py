6c28be3713ac81c0a95b2aa05536cf4669da303f
SchissTV <114029138+SchissTV@users.noreply.github.com>
2023-03-30 09:22:16 +0200

    s

---
+++ Test/OOP/trig.py
@@ -0,0 +1,16 @@
+import math
+
+class Trig:
+    def __init__(self, op, zahlen):
+        self.op = op
+        self.zahlen = zahlen
+        
+    def rechnen(self):
+        for i, op in enumerate(self.op):
+            if op == "sin":
+                self.zahlen[i]=round(math.sin(math.radians(self.zahlen[i])),3)
+            elif op == "cos":
+                self.zahlen[i]=round(math.cos(math.radians(self.zahlen[i])),3)
+            elif op == "tan":
+                self.zahlen[i]=round(math.tan(math.radians(self.zahlen[i])),3)
+            del self.op[i]
